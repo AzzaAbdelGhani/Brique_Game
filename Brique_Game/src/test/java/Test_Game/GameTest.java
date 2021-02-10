525c2a94b6893947d5a4daee3481a2ca99ada162
KEERTHANA CHANDRASEKAR <cj.keerthana96@gmail.com>
2021-02-10 13:23:31 +0100

    Test for Game Class: Proper Initilization of the Game, with Black playing first by default

---
+++ Brique_Game/src/test/java/Test_Game/GameTest.java
@@ -0,0 +1,21 @@
+package Test_Game;
+
+import Game.Piece_Color;
+import Game.Status;
+import Game.Game;
+import Game.Player;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class GameTest {
+        private Player P1 = new Player("P1", Piece_Color.BLACK);
+        private Player P2 = new Player("P2", Piece_Color.WHITE);
+        private Game game = new Game();
+
+        @Test
+        public void startingtheGame(){
+            game.startGame(P1,P2);
+            assertEquals(game.getStatus(),Status.ON);
+            assertEquals(game.getActivePlayer().getColor(),Piece_Color.BLACK);
+        }
+}
