f18ea0d59e2027d5d31bf7a4cc18c0132975f9f2
KEERTHANA CHANDRASEKAR <cj.keerthana96@gmail.com>
2021-02-10 01:43:25 +0100

    Other files

---
+++ Brique_Game/src/main/java/Game/Board.java
@@ -0,0 +1,30 @@
+package Game;
+
+public class Board {
+    private static final int size = 15;
+    private Position[][] grid = new Position[size][size];
+
+    public void initializeBoard(){
+        for(int i = 0; i < size; i++){
+            for (int j = 0; j < size; j++){
+                this.grid[i][j] = new Position(i,j,Pos_Color.LIGHT);
+                this.grid[i][j].setPieceColor(Piece_Color.BLANK);
+                if((i+j)%2 != 0){
+                    this.grid[i][j].setPosColor(Pos_Color.DARK);
+                }
+            }
+        }
+    }
+
+    public void reset(){ initializeBoard(); }
+
+    public int getSize() { return size; }
+
+    public void fillPos(int i, int j, Piece_Color pColor){
+        this.grid[i][j].setPieceColor(pColor);
+    }
+
+    public Piece_Color getPosFill(int i, int j){
+        return this.grid[i][j].getPieceColor();
+    }
+}
