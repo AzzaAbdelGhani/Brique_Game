cf1fa4cb2778c3b0a121fc02cea304b339f43cda
AzzaAbdelGhani <azza.mazher90@gmail.com>
2021-02-10 16:49:56 +0100

    add Graph class, which represents each player's movements

---
+++ Brique_Game/src/main/java/Game/Graph.java
@@ -0,0 +1,65 @@
+package Game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graph {
+
+    private Piece_Color pieceColor;
+    private List<List<Integer>> Adjacency_List;
+
+    public Graph(Piece_Color pieceColor)
+    {
+        this.pieceColor = pieceColor;
+        Adjacency_List = new ArrayList<List<Integer>>();
+        for (int i = 0; i < 225; i++)
+        {
+            ArrayList<Integer> new_list = new ArrayList<>();
+            Adjacency_List.add(i,new_list);
+        }
+    }
+
+    public int get_Index(int row, int col) {    return row*15 + col ;   }
+
+    public void add_node(int row, int col)
+    {
+        int idx = get_Index(row, col);
+        Adjacency_List.get(idx).add(idx);
+    }
+
+    public void printGraph()
+    {
+        System.out.println ("Adjacency List for the graph \n");
+        for (List<Integer> l : Adjacency_List)
+        {
+            if(!l.isEmpty()) {
+                for (int i = 0; i < l.size(); i++) {
+                    System.out.print("->" + l.get(i));
+                }
+                System.out.print("\n");
+            }
+        }
+    }
+
+    public static void main(String...arg)
+    {
+        Graph adjacencyList = new Graph(Piece_Color.BLACK);
+        adjacencyList.add_node(10, 7);
+        adjacencyList.add_node(8, 7);
+        adjacencyList.add_node(0, 7);
+        adjacencyList.add_node(6, 7);
+        adjacencyList.add_node(14, 7);
+        adjacencyList.add_node(1, 7);
+        adjacencyList.add_node(5, 7);
+        adjacencyList.add_node(2, 7);
+        adjacencyList.add_node(7, 7);
+        adjacencyList.add_node(3, 7);
+        adjacencyList.add_node(12, 7);
+        adjacencyList.add_node(4, 7);
+        adjacencyList.add_node(13, 7);
+        adjacencyList.add_node(9, 7);
+        adjacencyList.add_node(11, 7);
+
+        adjacencyList.printGraph();
+    }
+}
--- Brique_Game/src/main/java/Game/Player.java
+++ Brique_Game/src/main/java/Game/Player.java
@@ -7,16 +7,19 @@ public class Player {
     private String name;
     private Piece_Color pieceColor; //each player choose a color to play with
     private Boolean active = Boolean.FALSE;
+    private Graph graph; // each player has a graph represents his movements
 
     public Player (String name, Piece_Color pieceColor)
     {
         this.name = name;
         this.pieceColor = pieceColor;
+        graph = new Graph(pieceColor);
     }
 
     public String getName() {return name;}
     public Piece_Color getColor() {return pieceColor;}
     public Boolean IsActive() {return active;}
+    public Graph getGraph() { return graph; }
 
     public void setActive(Boolean active) {this.active = active;}
 }
