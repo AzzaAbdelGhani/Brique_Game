6ebbff46001d62528aded97c8194a2eb1a3a08ca
KEERTHANA CHANDRASEKAR <cj.keerthana96@gmail.com>
2021-02-12 11:12:36 +0100

    Testing startGame(), scanCoordinatesandFill() methods

--- Brique_Game/src/test/java/Test_Game/GameTest.java
+++ Brique_Game/src/test/java/Test_Game/GameTest.java
@@ -6,6 +6,10 @@ import Game.Game;
 import Game.Player;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class GameTest {
         private Player P1 = new Player("P1", Piece_Color.BLACK);
@@ -17,5 +21,16 @@ public class GameTest {
             game.startGame(P1,P2);
             assertEquals(game.getStatus(),Status.ON);
             assertEquals(game.getActivePlayer().getColor(),Piece_Color.BLACK);
+        }
+
+        @Test
+        public void fillBoard(){
+            game.startGame(P1,P2);
+            InputStream backup = System.in;
+            System.setIn(new ByteArrayInputStream("-1 2 3 4".getBytes()));
+            assertEquals(P1.getColor(),game.scanCoordinatesandFill(P1.getName(),P1.getColor()));
+            System.setIn(new ByteArrayInputStream("3 4 5 6".getBytes()));
+            assertEquals(P2.getColor(),game.scanCoordinatesandFill(P2.getName(),P2.getColor()));
+            System.setIn(backup);
         }
 }
