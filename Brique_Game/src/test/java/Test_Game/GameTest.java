a61ca0f73912a603a308d5d86b7f1eeb98f0425a
KEERTHANA CHANDRASEKAR <cj.keerthana96@gmail.com>
2021-02-13 00:11:21 +0100

    Refactor GameTest Class: Included tests for getting active Players and trying to write a test for playing the game

--- Brique_Game/src/test/java/Test_Game/GameTest.java
+++ Brique_Game/src/test/java/Test_Game/GameTest.java
@@ -24,7 +24,20 @@ public class GameTest {
         }
 
         @Test
-        public void fillBoard(){
+        public void activePlayers(){
+            P1.setActive(true);
+            P2.setActive(false);
+            game.startGame(P1,P2);
+            assertEquals(P1, game.getActivePlayer());
+            assertEquals(P2, game.getOtherPlayer());
+        }
 
+        @Test
+        public void playingtheGame(){
+            String moves = "1 1 3 4 2 2 6 7 3 3 1 5 4 4 1 6 5 5 1 8 6 6 1 10 7 7 1 11 7 8 14 15 7 9 2 5 7 10 10 5 8 11 13 8 9 12 6 1 9 13 6 2 9 13 4 9 10 14 2 9 10 15 12 15";
+            InputStream backup = System.in;
+            System.setIn(new ByteArrayInputStream(moves.getBytes()));
+            game.startGame(P1,P2);
+            //assertEquals(Status.P1_WINS,game.Play());
         }
 }
