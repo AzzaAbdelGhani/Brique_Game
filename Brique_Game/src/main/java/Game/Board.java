bf88402b93bd01cbb530785b4ae2b8ede106dc35
KEERTHANA CHANDRASEKAR <cj.keerthana96@gmail.com>
2021-02-12 13:48:59 +0100

    Refactor Board Class: Making Escort Rules

--- Brique_Game/src/main/java/Game/Board.java
+++ Brique_Game/src/main/java/Game/Board.java
@@ -25,9 +25,45 @@ public class Board {
         return this.grid[i][j].getPieceColor();
     }
 
+    public Pos_Color getPosColor(int i, int j) { return  this.grid[i][j].getPosColor(); }
+
     public boolean isValidPos(int i, int j) {
         if (i < 0 || j < 0 || i >= this.size || j >= this.size) { return Boolean.FALSE; }
         return Boolean.TRUE;
+    }
+
+    public void fillEscorts_interior(int i, int j){
+        Piece_Color current = getPosFill(i,j);
+        Piece_Color diag_up = getPosFill(i+1,j+1);
+        Piece_Color diag_down = getPosFill(i-1,j-1);
+        if (getPosColor(i,j) == Pos_Color.LIGHT){
+            if (current == diag_up){ fillPos(i,j+1, current); }
+            if (current == diag_down){ fillPos(i-1,j,current); }
+        }
+        else {
+            if (current == diag_up){ fillPos(i+1,j,current); }
+            if (current == diag_down){ fillPos(i,j-1,current);}
+        }
+    }
+
+    public void fillEscorts_edges(int i, int j){
+        Pos_Color posColor = getPosColor(i,j);
+        Piece_Color current = getPosFill(i,j);
+        if (i == 0 || j == 0){
+            Piece_Color diag_up = getPosFill(i+1,j+1);
+            if (current == diag_up){
+                if(posColor == Pos_Color.DARK) { fillPos(i+1,j,current); }
+                else { fillPos(i,j+1,current); }
+            }
+        }
+        if (i == size-1 || j == size-1){
+            Piece_Color diag_down = getPosFill(i-1,j-1);
+            if (current == diag_down){
+                if(posColor == Pos_Color.DARK) { fillPos(i,j-1,current); }
+                else { fillPos(i-1,j,current); }
+            }
+        }
+        if (i == size-1 && posColor == Pos_Color.DARK) { fillPos(i,j+1,current); }
     }
 
     public void printBoard(){
