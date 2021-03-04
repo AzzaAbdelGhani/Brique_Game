8dfb9332862c0359bf329cc05181bd4094731755
Cjkeerthana <56100791+Cjkeerthana@users.noreply.github.com>
2021-03-04 15:33:48 +0100

    Refactor Board Class: intializeBoard method and is ValidPos change with range values instead for loops and constants

--- Brique_Game/src/main/java/Game/Board.java
+++ Brique_Game/src/main/java/Game/Board.java
@@ -1,36 +1,28 @@
 package Game;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Board {
     private static final int size = 15;
     private Position[][] grid = new Position[size][size];
 
-    public void initializeBoard(){
-        for(int i = 0; i < size; i++){
-            for (int j = 0; j < size; j++){
-                this.grid[i][j] = new Position(i,j,Pos_Color.LIGHT);
-                if((i+j)%2 != 0){ this.grid[i][j].setPosColor(Pos_Color.DARK); }
-            }
-        }
-    }
-
-    //public void reset(){ initializeBoard(); }
+    public void initializeBoard(){ IntStream.range(0,size).forEach(i->IntStream.range(0,size).forEach(j->this.grid[i][j] = new Position(i,j))); }
 
     public int getSize() { return size; }
 
     public Position getPos(int r, int c) {return this.grid[r][c];}
 
-    public void fillPos(int i, int j, Piece_Color pColor){
-        this.grid[i][j].setPieceColor(pColor);
-    }
-
-    public Piece_Color getPosFill(int i, int j){
-        return this.grid[i][j].getPieceColor();
-    }
+    public Piece_Color getPosFill(int i, int j){ return this.grid[i][j].getPieceColor(); }
 
     public Pos_Color getPosColor(int i, int j) { return  this.grid[i][j].getPosColor(); }
 
+    public void fillPos(int i, int j, Piece_Color pColor){ this.grid[i][j].setPieceColor(pColor); }
+
     public boolean isValidPos(int i, int j) {
-        if (i < 0 || j < 0 || i >= this.size || j >= this.size) { return Boolean.FALSE; }
-        return Boolean.TRUE;
+        List<Integer> range = IntStream.rangeClosed(0,size).boxed().collect(Collectors.toList());
+        if(range.contains(i) && range.contains(j)) { return Boolean.TRUE; }
+        return Boolean.FALSE;
     }
 }
