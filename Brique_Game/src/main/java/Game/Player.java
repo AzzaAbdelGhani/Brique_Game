31bd850f31330759a8aa84625fdc93bbb5afbfbd
Cjkeerthana <56100791+Cjkeerthana@users.noreply.github.com>
2021-03-04 16:14:47 +0100

    Refactor Player Class: rename setPieceColor to Color as the same name exists in Position Class also

--- Brique_Game/src/main/java/Game/Player.java
+++ Brique_Game/src/main/java/Game/Player.java
@@ -13,7 +13,7 @@ public class Player {
     {
         this.name = name;
         this.pieceColor = pieceColor;
-        graph = new Graph(pieceColor);
+        this.graph = new Graph(pieceColor);
     }
 
     public String getName() {return name;}
@@ -23,7 +23,7 @@ public class Player {
     public void updateGraph(Board board, int x, int y) { this.graph.updateBoard(board, x, y); }
     public boolean checkPath() { return this.graph.areBordersConnected(); }
     public void setName(String name) {this.name = name;}
-    public void setPieceColor(Piece_Color pieceColor){this.pieceColor = pieceColor;}
+    public void setColor(Piece_Color pieceColor){this.pieceColor = pieceColor;}
     public void setActive(Boolean active) {this.active = active;}
     public void setGraph(Graph graph){this.graph = graph;}
 }
