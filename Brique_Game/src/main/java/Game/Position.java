a48dacc6c988249cfcf5c01b3a363a692b3dc06b
Cjkeerthana <56100791+Cjkeerthana@users.noreply.github.com>
2021-03-10 14:34:09 +0100

    Refactor Position Class: include coordinates instead of row id and col id

--- Brique_Game/src/main/java/Game/Position.java
+++ Brique_Game/src/main/java/Game/Position.java
@@ -1,33 +1,23 @@
 package Game;
 
 public class Position {
-    private int row_idx;
-    private int col_idx;
+    private Coordinates coordinates;
     private Pos_Color posColor;
     private Piece_Color pColor = Piece_Color.BLANK;
 
     public Position(int row_idx, int col_idx){
-        this.row_idx = row_idx;
-        this.col_idx = col_idx;
+        coordinates = new Coordinates(row_idx, col_idx);
         if ((row_idx + col_idx) % 2 == 0) this.posColor = Pos_Color.LIGHT;
         else this.posColor = Pos_Color.DARK;
     }
 
-    public int getRow_idx(){
-        return this.row_idx;
-    }
-
-    public int getCol_idx(){ return this.col_idx; }
+    public Coordinates getCoordinates() { return  this.coordinates;}
 
     public Pos_Color getPosColor(){
         return this.posColor;
     }
 
     public Piece_Color getPieceColor(){ return this.pColor; }
-
-    public void setPosColor(Pos_Color posColor) {
-        this.posColor = posColor;
-    }
 
     public void setPieceColor(Piece_Color pColor) {
         this.pColor = pColor;
--- Brique_Game/src/test/java/Test_Game/PositionTest.java
+++ Brique_Game/src/test/java/Test_Game/PositionTest.java
@@ -1,30 +1,33 @@
 package Test_Game;
 
+import Game.Coordinates;
 import Game.Piece_Color;
 import Game.Pos_Color;
 import Game.Position;
 import org.junit.jupiter.api.Test;
+
+import java.awt.image.ColorConvertOp;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PositionTest {
 
     private Position po = new Position(0,0);
+    private Position po2 = new Position(0,1);
 
     @Test
     public void checkRowIdColId(){
-        assertEquals(po.getRow_idx(),0);
-        assertEquals(po.getCol_idx(), 0);
+        assertEquals(po.getCoordinates().getX(),0);
+        assertEquals(po.getCoordinates().getY(), 0);
         assertEquals(po.getPieceColor(), Piece_Color.BLANK);
         assertEquals(po.getPosColor(), Pos_Color.LIGHT);
+        assertEquals(po2.getPosColor(), Pos_Color.DARK);
     }
 
     @Test
     public void checkPieceColorPosColor(){
         po.setPieceColor(Piece_Color.BLACK);
         assertEquals(po.getPieceColor(), Piece_Color.BLACK);
-        po.setPosColor(Pos_Color.DARK);
-        assertEquals(po.getPosColor(),Pos_Color.DARK);
     }
 
 }
